30c8cc59b0445898dd6568ec8f31d51a904c2acc
Tez28 <courtezcannady@gmail.com>
2022-03-05 11:46:10 -0500

    initial apuroutes

---
+++ routes/apiroutes.js
@@ -0,0 +1,14 @@
+const router = require('express').Router();
+const fs = require('fs');
+
+const db = require('../db/db.json');
+
+router.get("/notes", (req,res) => {
+    
+});
+
+router.post("/notes", (req,res) => {
+    
+});
+
+module.exports = router
