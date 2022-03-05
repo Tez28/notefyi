f557e560cb555c8e21c7983d2f333bf9d614ae6e
Tez28 <courtezcannady@gmail.com>
2022-03-05 11:42:23 -0500

    parser JSON and String

--- server.js
+++ server.js
@@ -6,3 +6,8 @@ const htmlroutes = require('./routes/htmlroutes');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON data
+app.use(express.json());
+
