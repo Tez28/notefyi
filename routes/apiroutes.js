e7d836c207cb2e5f5033cf279c35df5046693b13
Tez28 <courtezcannady@gmail.com>
2022-03-06 12:21:03 -0500

    get and post

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -4,11 +4,35 @@ const fs = require('fs');
 const db = require('../db/db.json');
 
 router.get("/notes", (req,res) => {
+    fs.readFile('../db/db.json', (err, data) => {
+        if (err) throw err;
+        dbData = JSON.parse(data);
+        res.send(dbData);
+    });
     
 });
 
 router.post("/notes", (req,res) => {
-    
+    const inputNotes = req.body;
+
+    fs.readFile('./db/db.json', (err, data) => {
+        if (err) throw err;
+        dbData = JSON.parse(data);
+        dbData.push(userNotes);
+        let number = 1;
+        dbData.forEach((note, index) => {
+            note.id = number;
+            number++;
+            return dbData;
+        });
+        console.log(dbData);
+        dataString = JSON.stringify(dbData);
+
+        fs.writeFile('../db/db.json', dataString, (err, data) => {
+            if(err) throw err;
+        });
+    });
+    res.send('Note added!')
 });
 
 module.exports = router
