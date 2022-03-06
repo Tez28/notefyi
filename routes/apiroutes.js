882328554a25fa618a93e04205836ce8bfd58fae
Tez28 <courtezcannady@gmail.com>
2022-03-06 13:21:33 -0500

    noteroutes

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -1,38 +1,7 @@
 const router = require('express').Router();
-const fs = require('fs');
+const routeNotes = require('./routeNotes');
 
-const db = require('../db/db.json');
+router.use(routeNotes);
 
-router.get("/notes", (req,res) => {
-    fs.readFile('../db/db.json', (err, data) => {
-        if (err) throw err;
-        dbData = JSON.parse(data);
-        res.send(dbData);
-    });
-    
-});
 
-router.post("/notes", (req,res) => {
-    const inputNotes = req.body;
-
-    fs.readFile('./db/db.json', (err, data) => {
-        if (err) throw err;
-        dbData = JSON.parse(data);
-        dbData.push(userNotes);
-        let number = 1;
-        dbData.forEach((note, index) => {
-            note.id = number;
-            number++;
-            return dbData;
-        });
-        console.log(dbData);
-        dataString = JSON.stringify(dbData);
-
-        fs.writeFile('../db/db.json', dataString, (err, data) => {
-            if(err) throw err;
-        });
-    });
-    res.send('Note added!')
-});
-
-module.exports = router
+module.exports = router;
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const express = require('express');
 const apiroutes = require('./routes/apiroutes')
 const htmlroutes = require('./routes/htmlroutes');
-
+const path = require('path');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
